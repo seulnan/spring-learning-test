6c463ff5bae2cf1bad0fa109afc65d881c21e8a0
kim nan seul <154604350+seulnan@users.noreply.github.com>
2025-04-23 13:52:22 +0900

    QueryCreationTest 성공
    CrudRepository는 JpaRepository보다 기능이 적지만, Spring Data JPA의 메서드 네이밍 기반 쿼리 생성은 그대로 지원.
    
    즉, findByLastName, findByLastNameIgnoreCase, findByLastNameOrderByFirstNameDesc 같은 메서드는 자동구현됨

--- spring-data-jpa-1/initial/src/main/java/cholog/CustomerRepository.java
+++ spring-data-jpa-1/initial/src/main/java/cholog/CustomerRepository.java
@@ -3,6 +3,14 @@ package cholog;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByLastName(String lastName);
+
+    List<Customer> findByLastNameIgnoreCase(String lastName);
+
+    List<Customer> findByLastNameOrderByFirstNameDesc(String lastName);
 }
